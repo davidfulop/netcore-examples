2e1c4a21334aee23afe7024a46ec3455036cce69
David Fulop <28536613+davidfulop@users.noreply.github.com>
2018-06-08 14:17:45 +0100

    Add Versioning basics.

---
+++ Versioning/src/Versioning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace Versioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var asm = typeof(Program).GetTypeInfo().Assembly;
+            var infoVersion = asm.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            var fileVersion = asm.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
+            Console.WriteLine($"Informational version: {infoVersion}{Environment.NewLine}FileVersion: {fileVersion}");
+        }
+    }
+}
