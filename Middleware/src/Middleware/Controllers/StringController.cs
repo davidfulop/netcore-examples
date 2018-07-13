ec43155e99adc7febb10bc256417eacae5482a53
David Fulop <28536613+davidfulop@users.noreply.github.com>
2018-07-13 17:32:03 +0100

    Initial commit for Middleware project.

---
+++ Middleware/src/Middleware/Controllers/StringController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+        private static readonly Random _random = new Random();
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            var time = _random.Next(20, 1500);
+            await Task.Delay(time);
+            return time.ToString();
+        }
+    }
+}
